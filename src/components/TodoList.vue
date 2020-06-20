f077ef5ee6fe0253c60932aecc9e8df5ea2a59c4
jake <t.brightsky@gmail.com>
2020-06-20 23:49:10 +0900

    フィルター機能追加

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,44 @@
 <template>
   <section class="section">
+    <div class="level">
+      <!-- Left side -->
+      <div class="level-left">
+        <div class="level-item">
+          <p class="subtitle is-5">
+            <strong>{{ filteredTodos.length }}</strong> Todos
+          </p>
+        </div>
+        <div class="level-item">
+          <div class="field has-addons">
+            <p class="control">
+              <b-input v-model="searchText" placeholder="Search..." type="search" icon="search" />
+            </p>
+            <p class="control">
+              <button class="button">
+                Search
+              </button>
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- Right side -->
+      <div class="level-right">
+        <b-field>
+          <b-radio-button v-model="selectedStatus" name="status-filter" native-value="">
+            All
+          </b-radio-button>
+          <b-radio-button
+            v-for="status in statuses"
+            v-model="selectedStatus"
+            :native-value="status.code"
+            :key="status.code"
+            name="status-filter"
+          >
+            {{ status.label }}
+          </b-radio-button>
+        </b-field>
+      </div>
+    </div>
     <div class="container">
       <form @submit.prevent="addTodo">
         <b-field>
@@ -8,38 +47,36 @@
       </form>
     </div>
     <div class="container">
-      <div>
-        <b-table :data="todos" striped hoverable>
-          <template v-slot="todo">
-            <b-table-column field="name" label="name">
-              {{ todo.row.name }}
-            </b-table-column>
-            <b-table-column label="Status" centered width="120">
-              <b-select v-model="todo.row.status" placeholder="Status">
-                <option v-for="status in statuses" :value="status.code" :key="status.code">
-                  {{ status.label }}
-                </option>
-              </b-select>
-            </b-table-column>
-            <b-table-column label="Delete" centered width="80">
-              <b-button @click="deleteTodo(todo.index)">
-                <b-icon icon="trash" size="is-small"></b-icon>
-              </b-button>
-            </b-table-column>
-          </template>
+      <b-table :data="filteredTodos" striped hoverable>
+        <template v-slot="todo">
+          <b-table-column field="name" label="name">
+            {{ todo.row.name }}
+          </b-table-column>
+          <b-table-column label="Status" centered width="120">
+            <b-select v-model="todo.row.status" placeholder="Status">
+              <option v-for="status in statuses" :value="status.code" :key="status.code">
+                {{ status.label }}
+              </option>
+            </b-select>
+          </b-table-column>
+          <b-table-column label="Delete" centered width="80">
+            <b-button @click="deleteTodo(todo.index)">
+              <b-icon icon="trash" size="is-small"></b-icon>
+            </b-button>
+          </b-table-column>
+        </template>
 
-          <template slot="empty">
-            <section class="section">
-              <div class="content has-text-grey has-text-centered">
-                <p>
-                  <b-icon icon="sad-tear" size="is-large" />
-                </p>
-                <p>Nothing here.</p>
-              </div>
-            </section>
-          </template>
-        </b-table>
-      </div>
+        <template slot="empty">
+          <section class="section">
+            <div class="content has-text-grey has-text-centered">
+              <p>
+                <b-icon icon="sad-tear" size="is-large" />
+              </p>
+              <p>Nothing here.</p>
+            </div>
+          </section>
+        </template>
+      </b-table>
     </div>
   </section>
 </template>
@@ -59,7 +96,26 @@ export default {
         { name: 'Finish doing laundry', status: 'done' },
       ],
       newTodo: '',
+      selectedStatus: '',
+      searchText: '',
     };
+  },
+  computed: {
+    filteredTodos() {
+      if (this.selectedStatus === '' && this.searchText === '') {
+        return this.todos;
+      }
+      return this.todos.filter(todo => {
+        let display = true;
+        if (this.searchText !== '') {
+          display = todo.name.includes(this.searchText);
+        }
+        if (display && this.selectedStatus !== '') {
+          display = todo.status === this.selectedStatus;
+        }
+        return display;
+      });
+    },
   },
   methods: {
     addTodo() {
