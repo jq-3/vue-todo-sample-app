fcf0d62908fbfb978569bfdd6ad7df930492c63a
bita-tamada <hiroyuki_tamada@bita.jp>
2020-06-24 12:36:45 +0900

    todoのリスト部分を切り出し

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -37,52 +37,14 @@
       </form>
     </div>
     <div class="container">
-      <b-table :data="filteredTodos" striped hoverable>
-        <template v-slot="todo">
-          <b-table-column field="title" label="Title">
-            <div :class="['todo', { editing: todo.row == editedTodo }]">
-              <p class="control view" @dblclick="editTodo(todo.row)">{{ todo.row.title }}</p>
-              <input
-                class="control input edit"
-                type="text"
-                v-model="todo.row.title"
-                @blur="doneEdit(todo.row)"
-                @keyup.enter="doneEdit(todo.row)"
-                @keyup.esc="cancelEdit(todo.row)"
-              />
-            </div>
-          </b-table-column>
-          <b-table-column label="Status" centered width="120">
-            <b-select v-model="todo.row.status" placeholder="Status">
-              <option v-for="status in statuses" :value="status.code" :key="status.code">
-                {{ status.label }}
-              </option>
-            </b-select>
-          </b-table-column>
-          <b-table-column label="Delete" centered width="80">
-            <b-button @click="removeTodo(todo.row)">
-              <b-icon icon="trash" size="is-small"></b-icon>
-            </b-button>
-          </b-table-column>
-        </template>
-
-        <template slot="empty">
-          <section class="section">
-            <div class="content has-text-grey has-text-centered">
-              <p>
-                <b-icon icon="sad-tear" size="is-large" />
-              </p>
-              <p>Nothing here.</p>
-            </div>
-          </section>
-        </template>
-      </b-table>
+      <TodoTable :filteredTodos="filteredTodos" />
     </div>
   </section>
 </template>
 
 <script>
 import { provide } from '@vue/composition-api';
+import TodoTable from './TodoTable';
 import { useGetStatuses } from '../composables/use-get-statuses';
 import { useStatusStore } from '../stores/use-status-store';
 import statusKey from '../stores/use-status-key';
@@ -93,6 +55,7 @@ import { useTodoFilter } from '../composables/use-todo-filter';
 import { useTodoListActions } from '../composables/use-todo-list-actions';
 
 export default {
+  components: { TodoTable },
   setup() {
     const { getStatuses } = useGetStatuses();
     const statusStore = useStatusStore(getStatuses());
@@ -105,16 +68,7 @@ export default {
 
     const { selectedStatus, searchText, filteredTodos } = useTodoFilter();
 
-    const {
-      newTodo,
-      editedTodo,
-      beforeEditCache,
-      addTodo,
-      removeTodo,
-      editTodo,
-      doneEdit,
-      cancelEdit,
-    } = useTodoListActions();
+    const { newTodo, addTodo } = useTodoListActions();
 
     return {
       statuses,
@@ -123,34 +77,8 @@ export default {
       searchText,
       filteredTodos,
       newTodo,
-      editedTodo,
-      beforeEditCache,
       addTodo,
-      removeTodo,
-      editTodo,
-      doneEdit,
-      cancelEdit,
     };
   },
 };
 </script>
-
-<style scoped lang="scss">
-.todo {
-  & > .view {
-    display: initial;
-  }
-
-  &.editing > .view {
-    display: none;
-  }
-
-  & > .edit {
-    display: none;
-  }
-
-  &.editing > .edit {
-    display: initial;
-  }
-}
-</style>
---
+++ src/components/TodoTable.vue
@@ -0,0 +1,91 @@
+<template>
+  <b-table :data="filteredTodos" striped hoverable>
+    <template v-slot="todo">
+      <b-table-column field="title" label="Title">
+        <div :class="['todo', { editing: todo.row == editedTodo }]">
+          <p class="control view" @dblclick="editTodo(todo.row)">{{ todo.row.title }}</p>
+          <input
+            class="control input edit"
+            type="text"
+            v-model="todo.row.title"
+            @blur="doneEdit(todo.row)"
+            @keyup.enter="doneEdit(todo.row)"
+            @keyup.esc="cancelEdit(todo.row)"
+          />
+        </div>
+      </b-table-column>
+      <b-table-column label="Status" centered width="120">
+        <b-select v-model="todo.row.status" placeholder="Status">
+          <option v-for="status in statuses" :value="status.code" :key="status.code">
+            {{ status.label }}
+          </option>
+        </b-select>
+      </b-table-column>
+      <b-table-column label="Delete" centered width="80">
+        <b-button @click="removeTodo(todo.row)">
+          <b-icon icon="trash" size="is-small"></b-icon>
+        </b-button>
+      </b-table-column>
+    </template>
+
+    <template slot="empty">
+      <section class="section">
+        <div class="content has-text-grey has-text-centered">
+          <p>
+            <b-icon icon="sad-tear" size="is-large" />
+          </p>
+          <p>Nothing here.</p>
+        </div>
+      </section>
+    </template>
+  </b-table>
+</template>
+<script>
+import { inject } from '@vue/composition-api';
+import statusKey from '../stores/use-status-key';
+import { useTodoListActions } from '../composables/use-todo-list-actions';
+
+export default {
+  props: {
+    filteredTodos: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup() {
+    const statusStore = inject(statusKey);
+    const { statuses, statusesWithAll } = statusStore;
+    const { editedTodo, removeTodo, editTodo, doneEdit, cancelEdit } = useTodoListActions();
+
+    return {
+      statuses,
+      statusesWithAll,
+      editedTodo,
+      removeTodo,
+      editTodo,
+      doneEdit,
+      cancelEdit,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.todo {
+  & > .view {
+    display: initial;
+  }
+
+  &.editing > .view {
+    display: none;
+  }
+
+  & > .edit {
+    display: none;
+  }
+
+  &.editing > .edit {
+    display: initial;
+  }
+}
+</style>
